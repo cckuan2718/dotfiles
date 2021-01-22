0394d56b9746d13827935bb8ffe7bb358eafb9de
Chang, Chu-Kuan <cckuan@changchukuan.name>
2021-01-23 02:42:16 +0800

    add ~/.local/src/

---
+++ .local/src/dmenu/dmenu-5.0/config.h
@@ -0,0 +1,28 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+/* static int topbar = 1; */                     /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	/* "monospace:size=10" */
+	"Inconsolata-16"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	/* [SchemeNorm] = { "#bbbbbb", "#222222" }, */
+	/* [SchemeSel] = { "#eeeeee", "#005577" }, */
+	/* [SchemeOut] = { "#000000", "#00ffff" }, */
+	[SchemeNorm] = { "#a89984", "#282828" },
+	[SchemeSel]  = { "#282828", "#d79921" },
+	[SchemeOut]  = { "#d79921", "#282828" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
